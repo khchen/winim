59c8d45af7fc4095daef9c9285ed77cf47d0bee3
Encho 'zolern' Topalov <entopalov@gmail.com>
2019-01-25 17:19:59 +0200

    fix type definitions

--- winim/inc/shellapi.nim
+++ winim/inc/shellapi.nim
@@ -1700,7 +1700,9 @@ const
   STR_MYDOCS_CLSID* = "{450D8FBA-AD25-11D0-98A8-0800361B1103}"
   PSGUID_INTERNETSHORTCUT* = DEFINE_GUID(0x000214a0'i32, 0, 0, [0xc0'u8, 0, 0, 0, 0, 0, 0, 0x46])
   PSGUID_INTERNETSITE* = DEFINE_GUID(0x000214a1'i32, 0, 0, [0xc0'u8, 0, 0, 0, 0, 0, 0, 0x46])
+type
   iDefViewID* = IUnknown
+const
   IID_ICopyHookA* = DEFINE_GUID(0x000214ef'i32, 0, 0, [0xc0'u8, 0, 0, 0, 0, 0, 0, 0x46])
   IID_IShellCopyHookA* = IID_ICopyHookA
   IID_ICopyHookW* = DEFINE_GUID(0x000214fc'i32, 0, 0, [0xc0'u8, 0, 0, 0, 0, 0, 0, 0x46])
